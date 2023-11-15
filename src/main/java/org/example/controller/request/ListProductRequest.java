fee1072b365c6ee44f6290efd1e641679e1cf8bc
Luis Manuel Sala Espiell <lui@luis-sala.local>
2023-11-15 16:45:35 -0300

    Removed category validation on search so we can search all products

--- src/main/java/org/example/controller/request/ListProductRequest.java
+++ src/main/java/org/example/controller/request/ListProductRequest.java
@@ -1,12 +1,13 @@
 package org.example.controller.request;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class ListProductRequest extends PaginatedRequest {
-    @NotNull
     private Long category;
 }
